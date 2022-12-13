759663e63ad2c03e4a79e103e5e4dd6da20a6a75
prafuldalwi <prafuldalwi1998@gmail.com>
2022-12-13 22:48:24 +0530

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <div className="App">
+      <Navbar />
       <h1>Shopmart</h1>
     </div>
   )
