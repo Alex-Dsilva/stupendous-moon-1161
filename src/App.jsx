3c5e133ba32a6c336b64c1d5145f93db3f74ecad
prafuldalwi <prafuldalwi1998@gmail.com>
2022-12-16 14:13:12 +0530

    Footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import Login from './Pages/Login'
 import Signup from './Pages/Signup'
 import './App.css'
 import Navbar from './components/Navbar'
-import Homepage from "./Pages/Homepage/Homepage";
+import Footer from './components/Footer/Footer'
+// import Homepage from "./Pages/Homepage/Homepage";
 
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Homepage />
+      {/* <Homepage /> */}
+      <Footer />
     </div>
   )
 }
