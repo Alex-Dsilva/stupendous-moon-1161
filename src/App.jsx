13e66fc6f8c3e49939304364f0e18a9d81d11682
Vishal Raj <vishal.kumar4856@gmail.com>
2022-12-17 19:21:15 +0530

    product page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import { useState } from 'react'
-import { Route, Routes } from 'react-router-dom'
 import Login from './Pages/Login'
 import Signup from './Pages/Signup'
 import './App.css'
 import Navbar from './components/Navbar'
 import Homepage from './Pages/Homepage/Homepage'
+import TodayDeals from './Pages/Products/TodayDeals'
+import Products from './Pages/Products/Products'
 
 function App() {
   return (
     <div className="App">
-      <Homepage/>
+      {/* <Homepage/> */}
+      {/* <TodayDeals title={"Featured Outlet Deals"}/> */}
+      {/* <Products title={"New Arrivals"} searchQuery={"new items"}/> */}
+      {/* <Products title={"Bestselling"} searchQuery={"best"}/> */}
     </div>
   )
 }
