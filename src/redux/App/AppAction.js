96c405502402ed0c9f9f2d8d6eedd790b55021fb
Alex Dislva <alexdislva79@gmail.com>
2023-03-13 23:46:31 +0530

    updted reducer

--- src/redux/App/AppAction.js
+++ src/redux/App/AppAction.js
@@ -49,6 +49,16 @@ const getProductsByID = (id) => async(dispatch) => {
   });
 };
 
+const getProductReviews = (id) => async(dispatch) => {
+  dispatch({type:types.GET_SINGLE_PRODUCTS_REQUEST});
+  axios.get(`https://victorious-houndstooth-colt.cyclic.app/products/productReviews/${id}`).then((res) =>{
+    dispatch({type:types.GET_SINGLE_PRODUCTS_SUCCESS, payload:res.data.data});
+  }).catch( (error)=> {
+    console.error(error);
+    dispatch({type:types.GET_SINGLE_PRODUCTS_FAILURE, payload:error.data.message});
+    alert(error.data.message)
+  });
+};
 
 const addToCart=(payload)=>async(dispatch)=>{
   
