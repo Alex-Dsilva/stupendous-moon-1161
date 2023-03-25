25d186ef902cdee18b0865097cf3c23890a10c1b
Alex Dislva <alexdislva79@gmail.com>
2023-03-25 23:53:18 +0530

    updated navbar

--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -10,26 +10,10 @@ import ProductCard from '../../components/ProductCard';
 import ProductSkeleton from '../../components/ProductSkeleton';
 import {HiViewBoards} from 'react-icons/hi'
 import {FaBars} from 'react-icons/fa'
+import Deals from '../../utils/Deals'
 import { HamburgerIcon } from "@chakra-ui/icons";
+import catagorys from '../../utils/catagorys'
 
-const catagorys=[
-  {
-    name:'Electronics',
-    list:['Bluetooth Speaker', 'Headphones & Earphones', '3D Printers & Accessories', 'Video Games Accessories']
-  },
-  {
-    name:'Sports & Outdoors',
-    list:['E-Bikes', 'Scooters & Wheels', 'Fitness & Body Building']
-  },
-  {
-    name:'TV Boxes & Mini PCs',
-    list:['TV Boxes', 'Windows Mini PCs']
-  },
-  {
-    name:'Computers, Tablets & Accessories',
-    list:['Laptops', 'Gaming Laptops', 'Keyboard & Mouse']
-  },
-]
 
 const Products = ({ title }) => {
   const { products, isLoading } = useSelector(state => state.Appreducer);
@@ -80,6 +64,7 @@ const Products = ({ title }) => {
 
   }, [products]);
 
+  console.log(Deals)
 
   const handleAddToCart = (product) => {
     dispatch(addToCart(product));
---
+++ src/utils/catagorys.js
@@ -0,0 +1,21 @@
+const catagorys=[
+    {
+      name:'Electronics',
+      list:['Bluetooth Speaker', 'Headphones & Earphones', '3D Printers & Accessories', 'Video Games Accessories']
+    },
+    {
+      name:'Sports & Outdoors',
+      list:['E-Bikes', 'Scooters & Wheels', 'Fitness & Body Building']
+    },
+    {
+      name:'TV Boxes & Mini PCs',
+      list:['TV Boxes', 'Windows Mini PCs']
+    },
+    {
+      name:'Computers, Tablets & Accessories',
+      list:['Laptops', 'Gaming Laptops', 'Keyboard & Mouse']
+    },
+  ]
+
+
+export default catagorys
