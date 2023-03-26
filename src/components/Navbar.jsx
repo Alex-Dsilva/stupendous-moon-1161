a7500380e9e2054fb9d44767d3c1eb3a4594bed1
Alex Dislva <alexdislva79@gmail.com>
2023-03-26 23:54:54 +0530

    edited navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,14 +24,21 @@ import {
   PopoverFooter,
   PopoverArrow,
   Text,
+  Icon,
+  Badge,
 } from "@chakra-ui/react";
 import { ChakraProvider } from "@chakra-ui/react";
 import {
-  AiOutlineShoppingCart,
+  GrCart,
   AiOutlineHeart,
   AiFillHeart,
+  IoCartOutline,
   AiOutlineMenu,
   AiOutlineSearch,
+  IoCallOutline,
+  FiHelpCircle,
+  IoTicketOutline,
+  AiOutlineMessage,
   BsChevronDown,
   BsChevronUp,
   FiSmartphone
@@ -41,16 +48,23 @@ import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import CustomPopover from "./CustomPopover";
 import { useState } from "react";
+import { useTranslation } from "react-i18next";
 
 
+const languages = ['English', 'हिन्दी']
 
 function Navbar() {
-
+  const { t, i18n } = useTranslation()
   // const { auth } = useSelector(store => store);
   const [search, setSearch] = useState('')
   const { name } = useSelector(store => store.Authreducer);
   // const {name}=auth
   const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const handleLanguage = (lang) => {
+    // alert("lang")
+    i18n.changeLanguage(lang);
+  }
 
   return (
     <ChakraProvider>
@@ -64,66 +78,200 @@ function Navbar() {
         border={"2px solid #046381"}
         backgroundColor={"#046381"}
         top={"0"} zIndex={5} >
-        <Flex w='100%' justify='flex-end' gap='6' mr={10}>
-          <CustomPopover trigger={<Button>Popover 1</Button>} height='fit-content'>
-              <>
-              <Text mb='1' noOfLines={3} >Download app to get up to 70% off <br/> app-only deals and coupons worth $50</Text>
+        <Flex w='100%' justify='flex-end' align='center' gap='6' mr={10}>
+          <CustomPopover
+            trigger={(<Flex align='center' p='2'>
+              <FiSmartphone style={{ fontSize: "24px", color: 'white' }} />
+              <Text fontSize='13.5px' mr='1.5' color='white' >{t('Save $50 with App')}</Text>
+              {false ? <BsChevronDown style={{ color: 'white' }} /> : <BsChevronUp style={{ color: 'white' }} />}
+            </Flex>)}
+            height='fit-content'>
+            <>
+              <Text mb='1' noOfLines={3} >Download app to get up to 70% off <br /> app-only deals and coupons worth $50</Text>
               <Flex align='center' justify='center'>
-                <Image h='8rem' w='8rem' src='https://content1.geekbuying.com/V1.4/en/images/index_images/android_app.png' alt='qr Download'/>
+                <Image h='8rem' w='8rem' src='https://content1.geekbuying.com/V1.4/en/images/index_images/android_app.png' alt='qr Download' />
                 <Box p='1'>
-                <Image mt='2'mb='2' src='https://content1.geekbuying.com/V1.4/en/images/index_images/app_store.jpg' alt='qr Download'/>
-                <Image mb='2.5' src='https://content1.geekbuying.com/V1.4/en/images/index_images/google_play.jpg' alt='qr Download'/>
-                <Image mb='2.5' src='https://content1.geekbuying.com/V1.4/en/images/index_images/gallery.jpg' alt='qr Download'/>
+                  <Image mt='2' mb='2' src='https://content1.geekbuying.com/V1.4/en/images/index_images/app_store.jpg' alt='qr Download' />
+                  <Image mb='2.5' src='https://content1.geekbuying.com/V1.4/en/images/index_images/google_play.jpg' alt='qr Download' />
+                  <Image mb='2.5' src='https://content1.geekbuying.com/V1.4/en/images/index_images/gallery.jpg' alt='qr Download' />
                 </Box>
               </Flex>
-                </>
+            </>
           </CustomPopover>
-          <Popover placement='bottom-end'>
-            <PopoverTrigger >
-              <Flex align='center' p='2'>
-                <FiSmartphone style={{ fontSize: "24px", color:'white' }}/>
-                <Text fontSize='13.5px' mr='1.5' color='white' >Save $50 with App</Text>
-                {false ? <BsChevronDown style={{color:'white' }}/>:<BsChevronUp style={{color:'white' }}/>}
-              </Flex>
-            </PopoverTrigger>
-            <PopoverContent h='100px' overflowY='scroll' w='fit-content' m='auto' p='10px'>
-              <PopoverArrow />
-              <PopoverBody>
-                
-              </PopoverBody>
-              {/* <PopoverFooter>This is the footer</PopoverFooter> */}
-            </PopoverContent>
-          </Popover>
-          <Popover placement='bottom-end'>
-            <PopoverTrigger>
-              <Flex align='center' p='2' gap={1}>
+          <CustomPopover
+            trigger={(<Flex align='center' p='2' gap={1}>
               <Text fontSize='13.5px' color='white' >English</Text>
-              {true ? <BsChevronDown style={{color:'white' }}/>:<BsChevronUp style={{ color:'white' }}/>}
+              {true ? <BsChevronDown style={{ color: 'white' }} /> : <BsChevronUp style={{ color: 'white' }} />}
+            </Flex>)}
+            height='fit-content'>
+            <>
+              {
+                languages.map((language) => {
+                  return <Text mb='1' noOfLines={3} cursor='pointer' onClick={() => handleLanguage('chi')} p='2px 10px'>{language}</Text>
+                })
+              }
+            </>
+          </CustomPopover>
+
+          <CustomPopover
+            trigger={(<Flex align='center' p='1' gap={1}>
+              <Text fontSize='13.5px' color='white' >Support Center</Text>
+              {true ? <BsChevronDown style={{ color: 'white' }} /> : <BsChevronUp style={{ color: 'white' }} />}
+            </Flex>)}
+            height='fit-content'>
+            <>
+              <Flex p='1' gap='2' align='center' >
+                <AiOutlineMessage />
+                Live Chat
               </Flex>
-            </PopoverTrigger>
-            <PopoverContent>
-              <PopoverArrow />
-              <PopoverHeader>Confirmation!</PopoverHeader>
-              <PopoverBody>Are you sure you want to have that milkshake?</PopoverBody>
-            </PopoverContent>
-          </Popover>
-          <Popover placement='bottom-end'>
-            <PopoverTrigger>
-              <Flex align='center' p='2' gap={1}>
-                <Text fontSize='13.5px' color='white' >Support Center</Text>
-                {true ? <BsChevronDown style={{color:'white' }}/>:<BsChevronUp style={{color:'white' }}/>}
+              <Flex p='1' gap='2' align='center' >
+                <IoTicketOutline />
+                Ticket
               </Flex>
-            </PopoverTrigger>
-            <PopoverContent>
-              <PopoverArrow />
-              <PopoverHeader>Confirmation!</PopoverHeader>
-              <PopoverBody>Are you sure you want to have that milkshake?</PopoverBody>
-            </PopoverContent>
-          </Popover>
+              <Flex p='1' gap='2' align='center' >
+                <FiHelpCircle />
+                Help Center
+              </Flex>
+              <Flex p='1' gap='2' align='center' >
+                <IoCallOutline />
+                Call Us
+              </Flex>
+            </>
+          </CustomPopover>
         </Flex>
-        <Divider bg='white' variant='solid' h='1px' orientation='horizontal' />
-        <Box w='100%' border='1px solid' bg='blue.600' p='5'></Box>
-        <Box w='100%' border='1px solid' bg='green.500' p='5'></Box>
+        <Divider bg='white' mb='1' variant='solid' h='1px' orientation='horizontal' />
+        <Flex w='100%' justify='space-around' p='2' align='center'>
+          <Link to="/" ><Image w={['7rem', '7rem', '9rem']} h="50px" id="logo" src={logo} alt="" /></Link>
+          <Flex w='50%' align='flex-start' >
+            <Input
+              border="1px solid black"
+              borderRight="0"
+              borderLeftRadius="4px"
+              h="40px"
+              borderRightRadius="0"
+              bg="white"
+              type="text"
+              id="search"
+              placeholder={"Search by Keyword"}
+              onChange={(e) => setSearch(e.target.value)}
+            />
+            <Button
+              w="50px"
+              h="40px"
+              borderLeft="0"
+              borderLeftRadius="0"
+              borderRightRadius="4px"
+              bg="white"
+            >
+              <AiOutlineSearch style={{ fontSize: "27px", fontWeight: "800" }} />
+            </Button>
+          </Flex>
+          <Flex display={["none", "none", "flex"]} ml='-30px' gap='2' align="center">
+            <CustomPopover
+              trigger={(<Flex align='center' p='1' gap={1}>
+                <span style={{ fontSize: '14px', color: 'white', fontWeight: '600' }}>Ship to </span>
+                <Image w='2rem' border='1px solid white' src="https://upload.wikimedia.org/wikipedia/en/thumb/4/41/Flag_of_India.svg/800px-Flag_of_India.svg.png" alt='flag' />
+                <Text color='white' fontWeight='600' >/INR</Text>
+                {true ? <BsChevronDown style={{ color: 'white' }} /> : <BsChevronUp style={{ color: 'white' }} />}
+              </Flex>)}
+              height='fit-content'>
+              <>
+                <Flex p='1' gap='2' align='center' >
+                  <AiOutlineMessage />
+                  Live Chat
+                </Flex>
+                <Flex p='1' gap='2' align='center' >
+                  <IoTicketOutline />
+                  Ticket
+                </Flex>
+                <Flex p='1' gap='2' align='center' >
+                  <FiHelpCircle />
+                  Help Center
+                </Flex>
+                <Flex p='1' gap='2' align='center' >
+                  <IoCallOutline />
+                  Call Us
+                </Flex>
+              </>
+            </CustomPopover>
+            <Link display={["none", "none", "flex"]}  to="/login" style={{ textDecoration: "none" }} id="sign">
+              <Box color="#FFFFFF" fontWeight="600" >
+                {name ? name : "Sign In"}
+              </Box>
+            </Link>
+          </Flex>
+          <Box
+            size="sm"
+            variant="ghost"
+            colorScheme="blue"
+            position="relative"
+            display={["none", "none", "flex"]} 
+          >
+            <IoCartOutline size={20} style={{ height: '25px', width: '25px', color: 'white' }} />
+            {0 > 0 && (
+              <Box position="absolute" top="-12px" right="-6px">
+                <Badge borderRadius="full" fontSize='8' variant="solid" colorScheme="red">
+                  {0}
+                </Badge>
+              </Box>
+            )}
+          </Box>
+          <Box display={["block", "block", "none"]}>
+            <IconButton
+              icon={<AiOutlineMenu />}
+              fontSize="20px"
+              aria-label="Open Navigation Menu"
+              onClick={onOpen}
+            />
+            <Drawer isOpen={isOpen} onClose={onClose} placement="right">
+              <DrawerOverlay />
+              <DrawerContent>
+                <DrawerCloseButton />
+                <DrawerHeader>Navigation</DrawerHeader>
+                <DrawerBody p="10px" textAlign="left">
+
+                  <Flex w="100%" p="10px" justify="space-around" align="center">
+                    <Link to="/login" onClick={onClose} style={{ textDecoration: "none" }} id="sign"><Box color="#030202" fontWeight="600" >{name ? name : "Sign In"}</Box></Link>
+                    <Link to="/cart" onClick={onClose} style={{ textDecoration: "none" }} id="cart"><Box color="#000000"><GrCart style={{ fontSize: "22px", fontWeight: "900" }} /></Box></Link>
+                  </Flex>
+
+                  <Divider />
+                  <Box p="30px">
+                    <Link style={{ textDecoration: "none" }} to="/newproducts" id="new">
+                      <Box color="#1a1818" p="10px 0px" fontWeight="600" >New</Box>
+                    </Link>
+
+                    <Link style={{ textDecoration: "none" }} to="/bestproducts" id="best">
+                      <Box color="#1a1818" p="10px 0px" fontWeight="600">Bestselling</Box>
+                    </Link>
+
+                    <Link style={{ textDecoration: "none" }} to="/clearanceproducts" id="clear">
+                      <Box color="#1a1818" p="10px 0px" fontWeight="600">Clearance</Box>
+                    </Link>
+
+                    <Link style={{ textDecoration: "none" }} to="/todaydeals" id="deal">
+                      <Box color="#1a1818" p="10px 0px" fontWeight="600">Deals</Box>
+                    </Link>
+
+                    <Link style={{ textDecoration: "none" }} to="/payment" id="coup">
+                      <Box color="#1a1818" p="10px 0px" fontWeight="600">Wallet</Box>
+                    </Link>
+
+                    <Link style={{ textDecoration: "none" }} to="/apponlyproducts" id="ap">
+                      <Box color="#1a1818" p="10px 0px" fontWeight="600">App Only</Box>
+                    </Link>
+                  </Box>
+                </DrawerBody>
+              </DrawerContent>
+            </Drawer>
+          </Box>
+        </Flex>
+        <Flex w='100%' border='1px solid' bg='green.500' p='5'>
+          <Flex >
+            <AiOutlineMenu />
+          </Flex>
+
+        </Flex>
       </Box>
     </ChakraProvider>
   );
@@ -132,90 +280,7 @@ function Navbar() {
 
 export default Navbar;
 
-
-
-{/* <Link to="/" style={{ width: "150px", marginLeft: "20px" }}><Image w="150px" h="45px" id="logo" src={logo} alt="" /></Link>
-        <Box w="100%">
-          <Flex justify="space-around" align="center" ml={["0px", "100px", "100px"]} >
-            <Flex w={["50%", "50%", "70%"]} justify="center" align="center" >
-              <Input w="80%"
-                border="1px solid black"
-                borderRight="0"
-                borderLeftRadius="4px"
-                h="35px"
-                borderRightRadius="0"
-                bg="white"
-                type="text"
-                id="search"
-                placeholder={"Search by Keyword"}
-                onChange={(e) => setSearch(e.target.value)}
-              />
-              <Button
-                border="1px solid black"
-                w="50px"
-                h={["36px", "36.5px", "37px"]}
-                borderLeft="0"
-                borderLeftRadius="0"
-                borderRightRadius="4px"
-                bg="white"
-              >
-                <AiOutlineSearch style={{ fontSize: "35px", fontWeight: "800" }} />
-              </Button>
-            </Flex >
-            <Flex w={["15%", "20%", "15%"]} display={["none", "none", "flex"]} justify="space-around" align="center">
-              <Link to="/login" style={{ textDecoration: "none" }} id="sign"><Box color="#FFFFFF" fontWeight="600" >{name ? name : "Sign In"}</Box></Link>
-              <Link to="/cart" style={{ textDecoration: "none" }} id="cart"><Box color="#FFFFFF"><AiOutlineShoppingCart style={{ fontSize: "22px", fontWeight: "900" }} /></Box></Link>
-            </Flex>
-            <Box display={["block", "block", "none"]}>
-              <IconButton
-                icon={<AiOutlineMenu />}
-                fontSize="20px"
-                aria-label="Open Navigation Menu"
-                onClick={onOpen}
-              />
-              <Drawer isOpen={isOpen} onClose={onClose} placement="right">
-                <DrawerOverlay />
-                <DrawerContent>
-                  <DrawerCloseButton />
-                  <DrawerHeader>Navigation</DrawerHeader>
-                  <DrawerBody p="10px" textAlign="left">
-
-                    <Flex w="100%" p="10px" justify="space-around" align="center">
-                      <Link to="/login" onClick={onClose} style={{ textDecoration: "none" }} id="sign"><Box color="#030202" fontWeight="600" >{name ? name : "Sign In"}</Box></Link>
-                      <Link to="/cart" onClick={onClose} style={{ textDecoration: "none" }} id="cart"><Box color="#000000"><AiOutlineShoppingCart style={{ fontSize: "22px", fontWeight: "900" }} /></Box></Link>
-                    </Flex>
-
-                    <Divider />
-                    <Box p="30px">
-                      <Link style={{ textDecoration: "none" }} to="/newproducts" id="new">
-                        <Box color="#1a1818" p="10px 0px" fontWeight="600" >New</Box>
-                      </Link>
-
-                      <Link style={{ textDecoration: "none" }} to="/bestproducts" id="best">
-                        <Box color="#1a1818" p="10px 0px" fontWeight="600">Bestselling</Box>
-                      </Link>
-
-                      <Link style={{ textDecoration: "none" }} to="/clearanceproducts" id="clear">
-                        <Box color="#1a1818" p="10px 0px" fontWeight="600">Clearance</Box>
-                      </Link>
-
-                      <Link style={{ textDecoration: "none" }} to="/todaydeals" id="deal">
-                        <Box color="#1a1818" p="10px 0px" fontWeight="600">Deals</Box>
-                      </Link>
-
-                      <Link style={{ textDecoration: "none" }} to="/payment" id="coup">
-                        <Box color="#1a1818" p="10px 0px" fontWeight="600">Wallet</Box>
-                      </Link>
-
-                      <Link style={{ textDecoration: "none" }} to="/apponlyproducts" id="ap">
-                        <Box color="#1a1818" p="10px 0px" fontWeight="600">App Only</Box>
-                      </Link>
-                    </Box>
-                  </DrawerBody>
-                </DrawerContent>
-              </Drawer>
-            </Box>
-          </Flex>
+{/*
 
           <Flex
             w="60%"
--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,22 @@
 
-import React from 'react'
+import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { Provider } from 'react-redux'
 import { store } from './redux/store'
+// import i18n from './utils/i18n'
+import './utils/i18n'
 import { BrowserRouter } from 'react-router-dom'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter >
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter> 
+      <Suspense fallback={(<div>Loading...</div>)}>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </Suspense>
+    </BrowserRouter >
 )
 
