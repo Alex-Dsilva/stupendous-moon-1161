402c969b76467d5209f9212994a0e17d01f5c07c
Alex Dislva <alexdislva79@gmail.com>
2023-03-27 23:15:50 +0530

    added

---
+++ src/utils/Countries.js
@@ -0,0 +1,168 @@
+const Countries = [
+    {
+      name: 'China',
+      flagImg: 'https://flagsapi.com/CN/flat/32.png'
+    },
+    {
+      name: 'India',
+      flagImg: 'https://flagsapi.com/IN/flat/32.png'
+    },
+    {
+      name: 'United States',
+      flagImg: 'https://flagsapi.com/US/flat/32.png'
+    },
+    {
+      name: 'Indonesia',
+      flagImg: 'https://flagsapi.com/ID/flat/32.png'
+    },
+    {
+      name: 'Pakistan',
+      flagImg: 'https://flagsapi.com/PK/flat/32.png'
+    },
+    {
+      name: 'Brazil',
+      flagImg: 'https://flagsapi.com/BR/flat/32.png'
+    },
+    {
+      name: 'Nigeria',
+      flagImg: 'https://flagsapi.com/NG/flat/32.png'
+    },
+    {
+      name: 'Bangladesh',
+      flagImg: 'https://flagsapi.com/BD/flat/32.png'
+    },
+    {
+      name: 'Russia',
+      flagImg: 'https://flagsapi.com/RU/flat/32.png'
+    },
+    {
+      name: 'Mexico',
+      flagImg: 'https://flagsapi.com/MX/flat/32.png'
+    },
+    {
+      name: 'Japan',
+      flagImg: 'https://flagsapi.com/JP/flat/32.png'
+    },
+    {
+      name: 'Ethiopia',
+      flagImg: 'https://flagsapi.com/ET/flat/32.png'
+    },
+    {
+      name: 'Philippines',
+      flagImg: 'https://flagsapi.com/PH/flat/32.png'
+    },
+    {
+      name: 'Egypt',
+      flagImg: 'https://flagsapi.com/EG/flat/32.png'
+    },
+    {
+      name: 'Vietnam',
+      flagImg: 'https://flagsapi.com/VN/flat/32.png'
+    },
+    {
+      name: 'DR Congo',
+      flagImg: 'https://flagsapi.com/CD/flat/32.png'
+    },
+    {
+      name: 'Turkey',
+      flagImg: 'https://flagsapi.com/TR/flat/32.png'
+    },
+    {
+      name: 'Iran',
+      flagImg: 'https://flagsapi.com/IR/flat/32.png'
+    },
+    {
+      name: 'Germany',
+      flagImg: 'https://flagsapi.com/DE/flat/32.png'
+    },
+    {
+      name: 'Thailand',
+      flagImg: 'https://flagsapi.com/TH/flat/32.png'
+    },
+  ];
+
+  const countriesCurrency = [
+    {
+      code: 'CNY',
+      symbol: '¥'
+    },
+    {
+      code: 'INR',
+      symbol: '₹'
+    },
+    {
+      code: 'USD',
+      symbol: '$'
+    },
+    {
+      code: 'IDR',
+      symbol: 'Rp'
+    },
+    {
+      code: 'PKR',
+      symbol: '₨'
+    },
+    {
+      code: 'BRL',
+      symbol: 'R$'
+    },
+    {
+      code: 'NGN',
+      symbol: '₦'
+    },
+    {
+      code: 'BDT',
+      symbol: '৳'
+    },
+    {
+      code: 'RUB',
+      symbol: '₽'
+    },
+    {
+      code: 'MXN',
+      symbol: '$'
+    },
+    {
+      code: 'JPY',
+      symbol: '¥'
+    },
+    {
+      code: 'ETB',
+      symbol: 'Br'
+    },
+    {
+      code: 'PHP',
+      symbol: '₱'
+    },
+    {
+      code: 'EGP',
+      symbol: '£'
+    },
+    {
+      code: 'VND',
+      symbol: '₫'
+    },
+    {
+      code: 'CDF',
+      symbol: 'FC'
+    },
+    {
+      code: 'TRY',
+      symbol: '₺'
+    },
+    {
+      code: 'IRR',
+      symbol: '﷼'
+    },
+    {
+      code: 'EUR',
+      symbol: '€'
+    },
+    {
+      code: 'THB',
+      symbol: '฿'
+    },
+  ];
+
+  export {Countries, countriesCurrency } 
+  
