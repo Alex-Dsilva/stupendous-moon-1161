39ad79105805d2b8d2fad72fd9367f5e58644ecb
Alex Dislva <alexdislva79@gmail.com>
2023-04-03 02:21:30 +0530

    added

--- src/Pages/Homepage/Homepage_copy.jsx
+++ src/Pages/Homepage/Homepage_copy.jsx
@@ -1,5 +1,5 @@
 import { StarIcon } from '@chakra-ui/icons'
-import { Box, Flex, Image, Text, IconButton, Grid , ChakraProvider, Tag, TagLabel, Progress } from '@chakra-ui/react'
+import { Box, Flex, Image, Text, IconButton, Grid, ChakraProvider, Tag, TagLabel, Progress } from '@chakra-ui/react'
 import React, { useState } from 'react'
 import { FaHeart, FaRegHeart } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
@@ -8,7 +8,7 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 // import { Navigation, Pagination } from "swiper";
 import Swiper_One from "./Swiper_one";
-import {Home} from '../../utils/catagorys'
+import { Home } from '../../utils/catagorys'
 
 
 
@@ -20,145 +20,111 @@ const Homepage = () => {
   // ((el.strikedprice - el.price) / el.strikedprice) * 100 
   const heartIcon = liked ? <FaHeart color="red" /> : <FaRegHeart />;
   const handleLike = async () => {
-      setLiked(!liked);
+    setLiked(!liked);
   };
 
-  const handleClick = () =>{
-      navigate(`/singleproduct${el._id}`)
+  const handleClick = () => {
+    navigate(`/singleproduct${el._id}`)
   }
 
   return (
     <ChakraProvider>
-    <Box className="Homepage">
-      <Flex gap={5}>
-        <Swiper_One />
-        <Box w='50%'  >
-          <Box p='5' mb='2px' border='2px solid #76767680'>
-            <Text as='b'>Deal of the Day</Text>
-            <Grid templateColumns='repeat(3,1fr)' gap={2} justifyContent='center'>
-            {Home.deal.map((el, i)=>{
-              return (
-                <Flex key={i} flexDir="column"  p={1} 
-              overflow="hidden"
-              justify='center'
-              align={'left'}
-              border='none'
-              // _hover={{ shadow:" rgba(100, 100, 111, 0.2) 0px 7px 29px 0px" }}
-              transition="all 0.3s ease-in-out" borderWidth="1px">
-            <Image alignSelf='left' h='100px' w='100px' src={`${el.images[0]}`} alt={el.name} />
-                    <Tag
-                      size={'sm'}
-                      key={'sm'}
-                      w='fit-content'
-                      borderRadius='full'
-                      variant='solid'
-                      colorScheme='red'
-                    >
-                      <TagLabel fontSize='11'>{(((el.strikedprice - el.price) / el.strikedprice) * 100).toFixed()}% off</TagLabel>
-                    </Tag>
-            <Text fontSize='15'>
-              <b>₹{el.price || 2549} </b> 
-            </Text>
-             <Text as='s' fontSize='13'>
-               ₹{el.strikedprice.toFixed(2) || 1249} 
-            </Text>
+      <Box className="Homepage">
+        <Flex gap={5} w='90vw'>
+          <Swiper_One />
+          <Box w='50%'  >
+            <Box p='2' mb='5px' border='2px solid #76767680'>
+              <Text fontSize={'19'} fontWeight={'400'} >Deal of the Day</Text>
+              <Grid templateColumns='repeat(3,1fr)' gap={2} justifyContent='center'>
+                {Home.deal.map((el, i) => {
+                  return (
+                    <Flex key={i} flexDir="column" gap='0.5' p={1}
+                      overflow="hidden"
+                      justify='center'
+                      align={'left'}
+                      border='none'
+                      // _hover={{ shadow:" rgba(100, 100, 111, 0.2) 0px 7px 29px 0px" }}
+                      transition="all 0.3s ease-in-out" borderWidth="1px">
+                      <Image alignSelf='left' h='100px' w='100px' src={`${el.images[0]}`} alt={el.name} />
+                      <Tag
+                        size={'sm'}
+                        key={'sm'}
+                        w='fit-content'
+                        borderRadius='full'
+                        variant='solid'
+                        colorScheme='red'
+                      >
+                        <TagLabel fontSize='11'>{(((el.strikedprice - el.price) / el.strikedprice) * 100).toFixed()}% off</TagLabel>
+                      </Tag>
+                      <Text fontSize='14'>
+                        <b>₹{el.price || 2549} </b>
+                      </Text>
+                      <Text as='s' fontSize='12'>
+                        ₹{el.strikedprice.toFixed(2) || 1249}
+                      </Text>
 
-             <Progress borderRadius={'50px'} colorScheme='blue' size='sm' value={20} />
-            <Text fontSize={'11'} color='gray.400'>20 Left</Text>
-            
-          </Flex>
-              )
-            })
+                      <Progress borderRadius={'50px'} h='1.5' colorScheme='blue' size='sm' value={20} />
+                      <Text fontSize={'11'} color='gray.400'>{el.qnty} Left</Text>
 
-            }
-          </Grid> 
+                    </Flex>
+                  )
+                })
 
+                }
+              </Grid>
+
+            </Box>
+
+            <Box p='2' border='2px solid #76767680'>
+              <Text fontSize={'19'} fontWeight={'400'} >Featured Products</Text>
+              <Grid templateColumns='repeat(3,1fr)' h='fit-content' gap={2} justifyContent='center'>
+                {Home.featured.map((el, i) => {
+                  return (
+                    <Flex key={i} flexDir="column" gap='0.5' p={1}
+                      overflow="hidden"
+                      justify='center'
+                      align={'left'}
+                      border='none'
+                      // _hover={{ shadow:" rgba(100, 100, 111, 0.2) 0px 7px 29px 0px" }}
+                      transition="all 0.3s ease-in-out" borderWidth="1px">
+                      <Image alignSelf='left' h='100px' w='100px' src={`${el.images[0]}`} alt={el.name} />
+                      <Tag
+                        size={'sm'}
+                        key={'sm'}
+                        w='fit-content'
+                        borderRadius='full'
+                        variant='solid'
+                        colorScheme='red'
+                      >
+                        <TagLabel fontSize='13'>{(((el.strikedprice - el.price) / el.strikedprice) * 100).toFixed()}% off</TagLabel>
+                      </Tag>
+                      <Text fontSize='15'>
+                        <b>₹{el.price || 2549} </b>
+                      </Text>
+                      <Text as='s' fontSize='13'>
+                        ₹{el.strikedprice.toFixed(2) || 1249}
+                      </Text>
+
+                      <Progress borderRadius={'50px'} h='1.5' colorScheme='blue' size='sm' value={20} />
+                      <Text fontSize={'11'} color='gray.400'>{el.qnty} Left</Text>
+
+                    </Flex>
+                  )
+                })
+
+                }
+              </Grid>
+
+            </Box>
           </Box>
+        </Flex>
+        <Box m='2'  border='1px solid' h='200'>
+          <Text fontSize={'17'} fontWeight={'500'}>Trending Categories</Text>
+                <Flex>
 
-          <Box p='5' border='2px solid #76767680'>
-            <Text as='b'>Deal of the Day</Text>
-            <Grid templateColumns='repeat(3,1fr)' h='fit-content' gap={2} justifyContent='center'>
-            {Home.deal.map((el, i)=>{
-              return (
-                <Flex key={i} flexDir="column"  p={1} 
-              overflow="hidden"
-              justify='center'
-              align={'left'}
-              border='none'
-              // _hover={{ shadow:" rgba(100, 100, 111, 0.2) 0px 7px 29px 0px" }}
-              transition="all 0.3s ease-in-out" borderWidth="1px">
-            <Image alignSelf='left' h='100px' w='100px' src={`${el.images[0]}`} alt={el.name} />
-                    <Tag
-                      size={'sm'}
-                      key={'sm'}
-                      w='fit-content'
-                      borderRadius='full'
-                      variant='solid'
-                      colorScheme='red'
-                    >
-                      <TagLabel fontSize='13'>{(((el.strikedprice - el.price) / el.strikedprice) * 100).toFixed()}% off</TagLabel>
-                    </Tag>
-            <Text fontSize='15'>
-              <b>₹{el.price || 2549} </b> 
-            </Text>
-             <Text as='s' fontSize='13'>
-               ₹{el.strikedprice.toFixed(2) || 1249} 
-            </Text>
-
-             <Progress borderRadius={'50px'} colorScheme='blue' size='sm' value={20} />
-            <Text fontSize={'11'} color='gray.400'>20 Left</Text>
-            
-          </Flex>
-              )
-            })
-
-            }
-          </Grid> 
-
-          </Box>
-          
-          
-          {/* <Grid templateColumns='repeat(3,1fr)' >
-            {Home.deal.map((el, i)=>{
-              return (
-                <Flex key={i} flexDir="column" gap={1} p={3} 
-              overflow="hidden"
-              onClick={handleClick}
-              justify='center'
-              align={'center'}
-              border='none'
-              _hover={{ shadow:" rgba(100, 100, 111, 0.2) 0px 7px 29px 0px" }}
-              transition="all 0.3s ease-in-out" borderWidth="1px">
-            <Image alignSelf='center' src={`${el.images[0]}`} alt={el.name} />
-
-            <Flex flexDir={'column'} wrap='wrap' w='80%'>
-                    <Tag
-                      size={'sm'}
-                      key={'sm'}
-                      w='fit-content'
-                      borderRadius='full'
-                      variant='solid'
-                      colorScheme='red'
-                    >
-                      <TagLabel>{(((el.strikedprice - el.price) / el.strikedprice) * 100).toFixed()}% off</TagLabel>
-                    </Tag>
-            <Text>
-              <b>₹{el.price || 2549} </b> 
-            </Text>
-             <Text as='s'>
-               ₹{el.strikedprice.toFixed(2) || 1249} 
-            </Text>
-            </Flex>
-            
-          </Flex>
-              )
-            })
-
-            }
-          </Grid> */}
+                </Flex>
         </Box>
-      </Flex>
-    </Box>
+      </Box>
     </ChakraProvider>
   );
 };
