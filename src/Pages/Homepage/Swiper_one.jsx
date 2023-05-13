865dee4216c0fc11dc1ec286926e0f8efa4c4270
Alex Dislva <alexdislva79@gmail.com>
2023-05-13 06:42:22 +0530

    fp05_405_day_4

--- src/Pages/Homepage/Swiper_one.jsx
+++ src/Pages/Homepage/Swiper_one.jsx
@@ -26,19 +26,19 @@ export default function Swiper_One() {
         className="mySwiper1"
       >
         <SwiperSlide>
-           <Image w='100%' src="https://img.gkbcdn.com/bn/2303/740x670-6426999f2b40c95014458299._p1_.jpg" alt="" />
+           <Image w='100%' h='100%' src="https://img.gkbcdn.com/bn/2303/740x670-6426999f2b40c95014458299._p1_.jpg" alt="" />
         </SwiperSlide>
         <SwiperSlide>
-            <Image w='100%' src="https://img.gkbcdn.com/bn/2303/740x670-641429812b40c9341017a70a._p1_.jpg" alt="" />
+            <Image w='100%' h='100%' src="https://img.gkbcdn.com/bn/2303/740x670-641429812b40c9341017a70a._p1_.jpg" alt="" />
         </SwiperSlide>
         <SwiperSlide>
-            <Image w='100%' src="https://img.gkbcdn.com/bn/2303/740x670-642699912b40c95014458298._p1_.jpg" alt="" />
+            <Image w='100%' h='100%' src="https://img.gkbcdn.com/bn/2303/740x670-642699912b40c95014458298._p1_.jpg" alt="" />
         </SwiperSlide>
         <SwiperSlide>
-        <Image w='100%' src="https://img.gkbcdn.com/s3/bn/2212/740x670-639825612b40c93b1474bd0b.jpg" alt="" />
+        <Image w='100%' h='100%' src="https://img.gkbcdn.com/s3/bn/2212/740x670-639825612b40c93b1474bd0b.jpg" alt="" />
         </SwiperSlide>
         <SwiperSlide>
-            <Image w='100%' src="https://img.gkbcdn.com/bn/2211/740x670-6384700b2b40c916cc6fe91c._p1_.jpg" alt="" />
+            <Image w='100%' h='100%' src="https://img.gkbcdn.com/bn/2211/740x670-6384700b2b40c916cc6fe91c._p1_.jpg" alt="" />
         </SwiperSlide>
       </Swiper>
     </>
